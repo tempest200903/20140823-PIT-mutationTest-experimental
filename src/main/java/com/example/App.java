4af56a2ca39e65ab1fbfee52d06cc7afd8dc2965
tempest200903 <tempest200903@gmail.com>
2014-08-23 23:04:08 +0900

    Run PIT Mutation Test in Eclipse Pitclipse

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,13 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public String foobar(int i) {
+		if (i >= 0) {
+			return "foo";
+		} else {
+			return "bar";
+		}
+	}
+
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,37 @@
 package com.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.hamcrest.CoreMatchers.is;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import static org.hamcrest.MatcherAssert.assertThat;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+	App app = new App();
+
+	@Test
+	public void testBar() {
+		{
+			String actual2 = app.foobar(-1);
+			assertThat(actual2, is("bar"));
+		}
+		{
+			String actual2 = app.foobar(-2);
+			assertThat(actual2, is("bar"));
+		}
+	}
+
+	@Test
+	public void testFoo() {
+		if (false) {
+			String actual = app.foobar(0);
+			assertThat(actual, is("foo"));
+		}
+		if (false) {
+			String actual = app.foobar(1);
+			assertThat(actual, is("foo"));
+		}
+	}
+
 }
